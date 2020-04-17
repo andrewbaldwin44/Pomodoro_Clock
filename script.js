0cca2534a65b03d220957e35293191fb02ab245c
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-04-17 17:52:11 -0400

    CSS style and mobile style.

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
 class Clock {
   constructor() {
-    this.arrowButtons = document.querySelectorAll('.arrowButton');
+    this.titleDisplay = document.querySelector('title');
+    this.clockDisplay = document.querySelector('#clockDisplay');
+    this.roundIndicatorDiv = document.querySelector('.roundIndicatorDiv');
     this.playButton = document.querySelector('#playButton');
-    this.stopButton = document.querySelector('#stopButton');
     this.pauseButton = document.querySelector('#pauseButton');
     this.restartButton = document.querySelector('#restartButton');
-    this.sessionTimeSelector = document.querySelector('#sessionTimeSelector');
-    this.breakTimeSelector = document.querySelector('#breakTimeSelector');
-    this.clockDisplay = document.querySelector('#clockDisplay');
-    this.clock = new Date();
-    this.roundCounter = 0;
-    this.startBreakChime = new Audio("assets/sounds/timer_end.mp3");
-    this.endBreakChime = new Audio("assets/sounds/big_daddy.mp3");
     this.settingsButton = document.querySelector('#settingsButton');
     this.settingsDiv = document.querySelector('.settingsDiv');
-    this.roundIndicatorDiv = document.querySelector('.roundIndicatorDiv');
+    this.sessionTimeSelector = document.querySelector('#sessionTimeSelector');
+    this.breakTimeSelector = document.querySelector('#breakTimeSelector');
+    this.arrowButtons = document.querySelectorAll('.arrowButton');
+
+    this.clock = new Date();
+
+    this.roundCounter = 0;
+
+    this.startBreakChime = new Audio("assets/sounds/timer_end.mp3");
+    this.endBreakChime = new Audio("assets/sounds/big_daddy.mp3");
   }
 
   initializeEventListeners() {
-    [...this.arrowButtons].map(arrowButton => {
-      arrowButton.addEventListener('click', () => this.updateSessionBreakTime(arrowButton));
-    });
-
     this.playButton.addEventListener('click', () => {
-      if(!this.playButton.classList.contains('disabled')) {
-        this.startClock();
-      }
+      if (!this.playButton.classList.contains('disabled')) this.startClock();
     });
 
     this.pauseButton.addEventListener('click', () => this.pauseClock());
 
     this.restartButton.addEventListener('click', () => {
       this.restartButton.classList.toggle('resetRotate');
-      this.restartButton.addEventListener('animationend', ()=> this.restartButton.classList.remove('resetRotate'));
-      this.restartClock();
+      this.restartButton.addEventListener('animationend', () => this.restartButton.classList.remove('resetRotate'));
+      this.restartCycle();
     });
 
-    this.settingsButton.addEventListener('click', ()=> this.toggleSettings());
+    this.settingsButton.addEventListener('click', () => this.toggleSettings());
+
+    [...this.arrowButtons].forEach(arrowButton => {
+      arrowButton.addEventListener('click', () => this.updateSessionBreakTime(arrowButton));
+      arrowButton.addEventListener('animationend', () => arrowButton.classList.remove('buttonShake'));
+    });
+
+    window.addEventListener("keydown", () => {
+      if (event.key == " ") {
+         !this.playButton.classList.contains('disabled') ? this.startClock() : this.pauseClock();
+      }
+    });
   }
 
   //Clock Functions
@@ -52,40 +60,50 @@ class Clock {
       this.seconds = this.padZeroes(this.clock.getSeconds());
       this.setClockDisplay(this.minutes, this.seconds);
       if (this.minutes <= 0 && this.seconds <= 0) this.switchSessionBreak();
-    }, 10);
+    }, 1000);
   }
 
   setClockValues() {
-    if(this.isPaused) return;
+    if (this.isPaused) return;
     if (this.cycleEnd()) {
-      this.clock.setMinutes(Number(this.breakTimeSelector.textContent)*2);
+      this.clock.setMinutes(Number(this.breakTimeSelector.textContent) * 2);
+    } else if (this.onBreak) {
+      this.clock.setMinutes(Number(this.breakTimeSelector.textContent));
     } else if (!this.onBreak) {
       this.clock.setMinutes(Number(this.sessionTimeSelector.textContent));
-    } else if (this.onBreak){
-      this.clock.setMinutes(Number(this.breakTimeSelector.textContent));
     }
     this.clock.setSeconds(0);
   }
 
-  setClockDisplay(minutes, seconds){
-    this.clockDisplay.textContent = `${minutes}:${seconds}`;
+  setClockDisplay(minutes, seconds) {
+    this.currentTime = `${minutes}:${seconds}`
+    this.clockDisplay.textContent = this.currentTime;
+    this.setTitleDisplay();
+  }
+
+  setTitleDisplay(){
+    if (focus) {
+      this.onBreak
+        ? this.titleDisplay.textContent = `(${this.currentTime}) Take A Break :)`
+        : this.titleDisplay.textContent = `(${this.currentTime}) Focus!`
+    } else this.titleDisplay.textContent = 'Pomodoro Clock';
   }
 
   padZeroes(time) {
     return String(time).length == 1 ? `0${time}` : time;
   }
 
-//Checks for onBreak switch to decide if user is on or off a break as clock ends a cycle
+  //Checks for onBreak switch to decide if user is on or off a break as clock ends a cycle
 
   switchSessionBreak() {
     clearInterval(this.countDown);
     if (this.cycleEnd()) {
-      this.restartClock();
-    } else if (this.onBreak  && !this.cycleEnd()) {
+      this.restartCycle();
+    } else if (this.onBreak) {
       this.onBreak = false;
       this.endBreakChime.play();
       this.startClock();
-    } else {
+    } else if (!this.onBreak) {
       this.roundCounter += 1;
       this.fillRoundCounter();
       this.onBreak = true;
@@ -95,11 +113,11 @@ class Clock {
   }
 
   fillRoundCounter() {
-    this.roundIndicatorDiv.children[this.roundCounter -1].classList.add('roundCompleted');
+    this.roundIndicatorDiv.children[this.roundCounter - 1].classList.add('roundCompleted');
   }
 
   resetRoundCounters() {
-    for(let i=0; i<this.roundIndicatorDiv.children.length; i++){
+    for (let i = 0; i < this.roundIndicatorDiv.children.length; i++) {
       this.roundIndicatorDiv.children[i].classList.remove('roundCompleted');
     }
   }
@@ -116,48 +134,48 @@ class Clock {
 
   restartClock(minutes = this.sessionTimeSelector.textContent) {
     this.playButton.classList.remove('disabled');
-    this.isPaused = false;
-    this.onBreak = false;
-    this.roundCounter = 0;
-    this.resetRoundCounters();
     clearInterval(this.countDown);
     this.setClockDisplay(minutes, '00');
   }
 
-  toggleSettings(){
+  restartCycle() {
+    this.restartClock();
+    this.isPaused = false;
+    this.onBreak = false;
+    this.roundCounter = 0;
+    this.resetRoundCounters();
+    this.titleDisplay.textContent = 'Pomodoro Clock';
+  }
+
+  toggleSettings() {
     this.settingsDiv.classList.toggle('show');
-    this.settingsButton.classList.toggle('buttonRotate');
+    this.settingsButton.classList.toggle('wrenchRotate');
+  }
+
+  toggleArrowButton(arrowButton, timeSelector = this.sessionTimeSelector.textContent) {
+    if (arrowButton.classList.contains('increment') && timeSelector < 60) return true;
+    else if (arrowButton.classList.contains('decrement') && timeSelector > 1) return true;
+    else arrowButton.classList.add('buttonShake');
   }
 
   updateSessionBreakTime(arrowButton) {
-    if (arrowButton.parentNode.parentNode.id === 'sessionSettings') {
-      if (this.sessionTimeSelector.textContent <= 1) arrowButton.disabled = true;
+    const settingsValueId = arrowButton.parentNode.parentNode.id;
+
+    if (settingsValueId == 'sessionSettings' && this.toggleArrowButton(arrowButton)) {
       this.sessionTimeSelector.textContent = Number(this.sessionTimeSelector.textContent) + Number(arrowButton.value);
-      if(!this.onBreak){
-        this.setClockDisplay(this.sessionTimeSelector.textContent, "00");
-      }
+      if (!this.onBreak) this.restartClock();
     }
-    if (arrowButton.parentNode.parentNode.id == "breakSettings") {
-      if (this.breakTimeSelector.textContent <= 1) return;
+    if (settingsValueId == "breakSettings" && this.toggleArrowButton(arrowButton, breakTimeSelector.textContent)) {
       this.breakTimeSelector.textContent = Number(this.breakTimeSelector.textContent) + Number(arrowButton.value);
-        if(this.onBreak) {
-          this.setClockDisplay(this.breakTimeSelector.textContent, "00");
-        }
+      if (this.onBreak) this.restartClock(this.breakTimeSelector.textContent);
     }
   }
 }
 
 new Clock().initializeEventListeners();
 
-window.onload = ()=> {
-  document.querySelector('body').classList.remove('noTransition');
-}
+window.onload = () => document.querySelector('body').classList.remove('noTransition');
 
-// window.onclick = e => {
-//   console.log(e.target)
-//   // console.log(e.target.contains(".fa-wrench"))
-//   if (!e.target.matches('#settingsButton') && !e.target.matches('.settingsDiv') && !e.target.matches('.sessionSettings')) {
-//     let settingsDiv = document.querySelector('.settingsDiv');
-//     if (settingsDiv.classList.contains('show')) settingsDiv.classList.remove('show');
-//   }
-// }
+let focus = false;
+window.onfocus = () => focus = false;
+window.onblur = () => focus = true;
